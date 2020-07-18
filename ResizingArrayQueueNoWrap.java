e10442378a77a444bc1ee950b25e5183010cd4d9
Riko Jacob @X260 <rikj@itu.dk>
2020-07-18 21:46:13 +0200

    code seems to work

---
+++ ResizingArrayQueueNoWrap.java
@@ -0,0 +1,181 @@
+/******************************************************************************
+ *  by Riko Jacob, rikj@itu.dk 
+ *  based on javac ResizingArrayQueue.java in algs4
+ *  Data files:   https://algs4.cs.princeton.edu/13stacks/tobe.txt  
+ *  
+ *  Queue implementation with a resizing array, resize to 2n when reaching the
+ *  end of the array.
+ *
+ ******************************************************************************/
+
+// package edu.princeton.cs.algs4;
+import edu.princeton.cs.algs4.*;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *  The {@code ResizingArrayQueue} class represents a first-in-first-out (FIFO)
+ *  queue of generic items.
+ *  It supports the usual <em>enqueue</em> and <em>dequeue</em>
+ *  operations, along with methods for peeking at the first item,
+ *  testing if the queue is empty, and iterating through
+ *  the items in FIFO order.
+ *  <p>
+ *  This implementation uses a resizing array, which creates a new underlying
+ *  array when the end of the array is reached (no wrap around). 
+ *  It halves the underlying array when it is one-quarter full.
+ *  The <em>enqueue</em> and <em>dequeue</em> operations take constant amortized time.
+ *  The <em>size</em>, <em>peek</em>, and <em>is-empty</em> operations takes
+ *  constant time in the worst case. 
+ *  <p>
+ * 
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *  and the document by Riko Jacob 
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ *  @author Riko Jacob
+ */
+public class ResizingArrayQueueNoWrap<Item> implements Iterable<Item> {
+    private Item[] q;       // queue elements
+    private int first;      // index of first element of queue
+    private int last;       // index of next available slot
+
+
+    /**
+     * Initializes an empty queue.
+     */
+    public ResizingArrayQueueNoWrap() {
+        q = (Item[]) new Object[4];
+        first = 0;
+        last = 0;
+    }
+
+    /**
+     * Is this queue empty?
+     * @return true if this queue is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return first == last;
+    }
+
+    /**
+     * Returns the number of items in this queue.
+     * @return the number of items in this queue
+     */
+    public int size() {
+        return last-first;
+    }
+
+    // resize the underlying array
+    private void resize() {
+        int n = last - first;
+        Item[] copy = (Item[]) new Object[2*n + 1];
+        for (int i = 0; i < n; i++) {
+            copy[i] = q[(first + i) ];
+        }
+        q = copy;
+        first = 0;
+        last  = n;
+    }
+
+    /**
+     * Adds the item to this queue.
+     * @param item the item to add
+     */
+    public void enqueue(Item item) {
+        // double size of array if necessary and recopy to front of array
+        if (last == q.length) resize();   // resize to double size of the current queue
+        q[last++] = item;                        // add item
+    }
+
+    /**
+     * Removes and returns the item on this queue that was least recently added.
+     * @return the item on this queue that was least recently added
+     * @throws java.util.NoSuchElementException if this queue is empty
+     */
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = q[first];
+        q[first] = null;                            // to avoid loitering
+        first++;
+        // shrink size of array if necessary
+        if ( last > first + 4 && last - first < q.length/4) resize(); 
+        return item;
+    }
+
+    /**
+     * Returns the item least recently added to this queue.
+     * @return the item least recently added to this queue
+     * @throws java.util.NoSuchElementException if this queue is empty
+     */
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return q[first];
+    }
+
+
+    /**
+     * Returns an iterator that iterates over the items in this queue in FIFO order.
+     * @return an iterator that iterates over the items in this queue in FIFO order
+     */
+    public Iterator<Item> iterator() {
+        return new ArrayIterator();
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ArrayIterator implements Iterator<Item> {
+        private int i = first;
+        public boolean hasNext()  { return i < last;                            }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = q[i];
+            i++;
+            return item;
+        }
+    }
+
+   /**
+     * Unit tests the {@code ResizingArrayQueue} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        ResizingArrayQueueNoWrap<String> queue = new ResizingArrayQueueNoWrap<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) queue.enqueue(item);
+            else if (!queue.isEmpty()) StdOut.print(queue.dequeue() + " ");
+        }
+        StdOut.println("(" + queue.size() + " left on queue)");
+    }
+
+}
+
+/******************************************************************************
+ *  Copyright 2002-2020, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
